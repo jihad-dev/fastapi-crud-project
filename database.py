8d1cdc701858da53bb22ff7ff61f9d824b8a7306
jihad-dev <mdjihadislam166@gmail.com>
2026-07-22 22:32:35 +0800

    postgres-setup

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-AQLALCHEMY_DATABASE_URL = "sqlite:///./todos_app.db"
-
-engine = create_engine(
-    AQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Format: postgresql://<USERNAME>:<PASSWORD>@<HOST>:<PORT>/<DATABASE_NAME>
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5432/todosAplication"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
